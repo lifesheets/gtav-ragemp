36a0503914c817e0f75263884462c8414370d22d
ua.lifesheets <ua.lifesheets@outlook.com>
2023-01-20 01:16:13 +0200

    Передача простих значень у браузер на Vue та у вигляді об'єкта.

--- client_packages/wixcore/modules/browser.js
+++ client_packages/wixcore/modules/browser.js
@@ -1,1 +1,13 @@
 "use strict"; // Created by ua.lifesheets on 01.01.2023.
+
+// Передача значений в браузер на VUE
+mp.transferBrowserSimple = function (text) {
+    browser.execute(text);
+}
+
+// Передача значений в VUE в виде объекта
+mp.transferBrowserObject = function (object) {
+    for (let objectKey in object) {
+        browser.execute(`${objectKey} = ${JSON.stringify(object[objectKey])}`);
+    }
+}
