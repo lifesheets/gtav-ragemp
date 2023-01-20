e1dc63e3234e2244e81fe8c46d06726cb97cdfe4
ua.lifesheets <ua.lifesheets@outlook.com>
2023-01-20 02:54:34 +0200

    Видалили з картки непотрібні IPLs.

--- packages/wixcore/events/index.js
+++ packages/wixcore/events/index.js
@@ -1,1 +1,3 @@
 "use strict"; // Created by ua.lifesheets on 01.01.2023.
+
+require('./world');
---
+++ packages/wixcore/events/world.js
@@ -0,0 +1,7 @@
+"use strict"; // Created by ua.lifesheets on 02.01.2023.
+
+mp.world.removeIpl('facelobbyfake');
+mp.world.removeIpl('fakeint');
+mp.world.removeIpl('fiblobbyfake');
+mp.world.removeIpl('jewel2fake');
+mp.world.removeIpl('sp1_10_fake_interior');
